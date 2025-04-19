9deb620635882cbe224be23a62818bbb1861150c
medi77na <medinastevenurregodavid@gmail.com>
2025-04-19 15:22:53 -0500

    fix: Conflicts resolved when merging unrelated records

--- EduLIM/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ EduLIM/Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1678450a65353dcb0b98dda1b71827188b6142df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
